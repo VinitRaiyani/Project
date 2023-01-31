7d3d1a2a7c0ead5ed6c2695ecc38499ab4d65990
vr <vinitraiyani12@gmail.com>
2023-01-31 18:31:15 +0530

    working on responsive part of deshboard

---
+++ HTML Training Assignment/js/java.js
@@ -0,0 +1,8 @@
+function myfunction(){
+    var x = document.getElementById("sideid");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
