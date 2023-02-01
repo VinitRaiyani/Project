c321b41d2d1459e861441ab5d33419c9e7ddcbde
vr <vinitraiyani12@gmail.com>
2023-02-01 18:30:19 +0530

    working on responsive part of analysis

--- HTML Training Assignment/js/java.js
+++ HTML Training Assignment/js/java.js
@@ -6,3 +6,12 @@ function myfunction(){
       x.style.display = "none";
     }
   }
+
+  function myfunction1(){
+    var x = document.getElementById("sideid1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
